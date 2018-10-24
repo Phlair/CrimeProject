eb6346914c012d1d1fde25867615cf852be7c5c6
George Claireaux <george@claireaux.co.uk>
2018-10-24 20:29:35 +0100

    Tableau main map with crimes complete, working on secondary data sheets

--- Model.sql
+++ Model.sql
@@ -7,16 +7,16 @@ GO
 --SELECT * FROM Drug.DeathbyRegion -- deaths by YEAR and REGION 
 --SELECT * FROM Drug.AdmissionsNHSRegion -- admissions by YEAR and REGION and GENDER
 --SELECT * FROM Drug.SeizuresForceArea -- seizures by YEAR and FORCE AREA
-SELECT * FROM Drug.SeizuresSnapshot17 -- seizures by FORCE AREA and DRUG (2017)
+--SELECT * FROM Drug.SeizuresSnapshot17 -- seizures by FORCE AREA and DRUG (2017)
 --SELECT * FROM Drug.SurveyData -- drug use by YEAR and REGION and DRUG
 --SELECT * FROM Geo.AreaCompare -- LSOA by FORCE AREA by REGION
 --SELECT * FROM Geo.DeprivationRanksLSOA -- deprivation rank by LSOA (England)
 --SELECT * FROM Geo.ForceArea -- area size and poly by FORCE AREA
 --SELECT * FROM Geo.LSOA -- area size and poly by LSOA
-SELECT * FROM Geo.PopulationLSOA -- population by YEAR and LSOA
+--SELECT * FROM Geo.PopulationLSOA -- population by YEAR and LSOA
 --SELECT * FROM Geo.WalesDeprivationRanksLSOA -- deprivation rank by LSOA (Wales)
-SELECT * FROM Police.CrimeDataStopSearch -- data by DATETIME(minute) and LSOA and CRIME TYPE (object) and GENDER
-SELECT * FROM Police.MatchedStreet -- data by YEAR and MONTH and LSOA and CRIME TYPE
+--SELECT * FROM Police.CrimeDataStopSearch -- data by DATETIME(minute) and LSOA and CRIME TYPE (object) and GENDER
+--SELECT * FROM Police.MatchedStreet -- data by YEAR and MONTH and LSOA and CRIME TYPE
 --SELECT * FROM Police.TaserUse -- taser incidents by YEAR and FORCE AREA
 --SELECT * FROM Weapons.BladedOffenceByAge -- minor/adult offences by YEAR and QUARTER
 --SELECT * FROM Weapons.BladedOffenceByArea -- offences by YEAR and FORCE AREA
@@ -28,14 +28,14 @@ SELECT * FROM Police.MatchedStreet -- data by YEAR and MONTH and LSOA and CRIME 
 --SELECT * FROM Weapons.FirearmOffenceByInjury -- nationwide injury types by YEAR
 --SELECT * FROM Weapons.FirearmOffenceByLocationType -- nationwide offence locations by YEAR
 --SELECT * FROM Weapons.FirearmOffenceByOffence -- nationwide offence types by YEAR
-SELECT * FROM Weapons.GunsAgeForceArea -- firearm/shotgun certificates by YEAR and FORCE AREA and AGE (age needs unpivoting)
-SELECT * FROM Weapons.GunsGenderForceArea -- firearm/shotgun certificates by YEAR and FORCE AREA and GENDER (gender needs unpivoting)
+--SELECT * FROM Weapons.GunsAgeForceArea -- firearm/shotgun certificates by YEAR and FORCE AREA and AGE (age needs unpivoting)
+--SELECT * FROM Weapons.GunsGenderForceArea -- firearm/shotgun certificates by YEAR and FORCE AREA and GENDER (gender needs unpivoting)
 --SELECT * FROM Weapons.ShotgunCertificatesForceArea -- certificate applications/totals and firearms by YEAR and FORCE AREA
 
 
-/*---------------------------------------------
-    Creating Dim.GeoEngland and Dim.GeoWales
-----------------------------------------------*/
+/*---------------------
+    Creating Dim.Geo
+----------------------*/
 -- For Region to ForceArea to LSOA
 	-- including deprivation indice ranks
 		-- 1 being the most deprived
@@ -108,9 +108,73 @@ JOIN Geo.WalesDeprivationRanksLSOA w
 ALTER TABLE Dim.GeoWales
 	Add id int identity primary key
 
+-- Combining the two (unfortunately lose some data but hey-ho)
+;with combinator AS (
+SELECT 
+	[Region name]
+	,[Region code]
+	,[Force area]
+	,[Force code]
+	,[Force size (sqkm)]
+	,[LSOA name]
+	,[LSOA code]
+	,[LSOA size (sqkm)]
+	,'England' as [Domain] -- because these are rank values so need to know domain of ranks
+	,[Multiple deprivation]
+	,[Income deprivation]
+	,[Employment deprivation]
+	,[Education deprivation]
+	,[Health deprivation]
+	,[Crime]
+	,[Barriers to housing/services]
+	,[Living environment dep]
+FROM dim.GeoEngland
+UNION
+SELECT
+	[Region name]
+	,[Region code]
+	,[Force area]
+	,[Force code]
+	,[Force size (sqkm)]
+	,[LSOA name]
+	,[LSOA code]
+	,[LSOA size (sqkm)]
+	,'Wales' as [Domain] -- because these are rank values so need to know domain of ranks
+	,[Multiple deprivation]
+	,[Income deprivation]
+	,[Employment deprivation]
+	,[Education deprivation]
+	,[Health deprivation]
+	,[Crime]
+	,[Barriers to housing]
+	,[Living environment dep]
+FROM dim.GeoWales )
+SELECT
+	*
+INTO dim.Geo
+FROM combinator
+-- Putting seperate tables in trash
+ALTER SCHEMA Trash TRANSFER Dim.GeoEngland
+ALTER SCHEMA Trash TRANSFER Dim.GeoWales
+-- Adding primary id key
+ALTER TABLE Dim.Geo
+	Add id int identity primary key
+
+
+/*--------------------
+   Creating Dim.Pop
+---------------------*/
+SELECT 
+	* 
+INTO Dim.Pop
+FROM geo.PopulationLSOA
+-- Adding primary id key
+ALTER TABLE Dim.Pop
+	Add id int identity primary key
+
 
 /*--------------------------
-    Creating Dim.DateTime
+    Creating Dim.Date
 ---------------------------*/
 -- CTE to union starting anchor date with dateadds
 ;WITH DatesUnion
@@ -118,44 +182,47 @@ AS
 (
 --anchor
 SELECT
-     CAST('2005-01-01' AS DATETIME) AS [DateTime] -- Main anchor point
-    ,YEAR(CAST('2005-01-01' AS DATETIME)) [Year] -- Derived year
+     CAST('2005-01-01' AS DATE) AS [AnchorDate] -- Anchor point
+    ,YEAR(CAST('2005-01-01' AS DATE)) [Year] -- Derived year
 	,CASE
-        WHEN MONTH(CAST('2005-01-01' AS DATETIME)) BETWEEN 3 AND 5
+        WHEN MONTH(CAST('2005-01-01' AS DATE)) BETWEEN 3 AND 5
         THEN 'Spring'
-        WHEN MONTH(CAST('2005-01-01' AS DATETIME)) BETWEEN 6 AND 8
+        WHEN MONTH(CAST('2005-01-01' AS DATE)) BETWEEN 6 AND 8
         THEN 'Summer'
-        WHEN MONTH(CAST('2005-01-01' AS DATETIME)) BETWEEN 9 AND 11
+        WHEN MONTH(CAST('2005-01-01' AS DATE)) BETWEEN 9 AND 11
         THEN 'Autumn'
         ELSE 'Winter'
      END AS [Season] -- Derived season
-	,DATEPART(QUARTER,CAST('2005-01-01' AS DATETIME)) [Quarter] -- Derived quarter
-    ,MONTH(CAST('2005-01-01' AS DATETIME)) [Month] -- Derived month
-    ,DATEPART(dd, CAST('2005-01-01' AS DATETIME)) [Day] -- Derived day (of month)
-    ,DATENAME(dw, (CAST('2005-01-01' AS DATETIME))) [DayName] -- Derived day (name)
+	,DATEPART(QUARTER,CAST('2005-01-01' AS DATE)) [Quarter] -- Derived quarter
+    ,MONTH(CAST('2005-01-01' AS DATE)) [Month] -- Derived month
+    /* ALL UNNECCESARY AS WE'RE ONLY GOING AS DEEP AS MONTH
+	,DATEPART(dd, CAST('2005-01-01' AS DATE)) [Day] -- Derived day (of month)
+    ,DATENAME(dw, (CAST('2005-01-01' AS DATE))) [DayName] -- Derived day (name)
     ,CASE
-        WHEN DATENAME(dw, (CAST('2005-01-01' AS DATETIME))) LIKE 'S%'
+        WHEN DATENAME(dw, (CAST('2005-01-01' AS DATE))) LIKE 'S%'
         THEN 0
         ELSE 1
      END as [WeekdayFlag] -- Is it a weekday? 1=y 0=n
 	 ,DATEPART(hh, CAST('2005-01-01' AS DATETIME)) [Hour] -- Derived hour
 	 ,DATEPART(mi, CAST('2005-01-01' AS DATETIME)) [Minute] -- Derived minute
+	 */
 UNION ALL
-SELECT -- Creating a new row for every 5 minute interval
-     DATEADD(mi, 5, [DateTime])
-    ,YEAR(DATEADD(mi, 5, [DateTime]))
+SELECT -- Creating a new row for every monthly interval
+     DATEADD(M, 1, AnchorDate) 
+    ,YEAR(DATEADD(M, 1, AnchorDate))
 	,CASE
-        WHEN MONTH(DATEADD(mi, 5, [DateTime])) BETWEEN 3 AND 5
+        WHEN MONTH(DATEADD(M, 1, AnchorDate)) BETWEEN 3 AND 5
         THEN 'Spring'
-        WHEN MONTH(DATEADD(mi, 5, [DateTime])) BETWEEN 6 AND 8
+        WHEN MONTH(DATEADD(M, 1, AnchorDate)) BETWEEN 6 AND 8
         THEN 'Summer'
-        WHEN MONTH(DATEADD(mi, 5, [DateTime])) BETWEEN 9 AND 11
+        WHEN MONTH(DATEADD(M, 1, AnchorDate)) BETWEEN 9 AND 11
         THEN 'Autumn'
         ELSE 'Winter'
      END AS season
-	,DATEPART(QUARTER,DATEADD(mi, 5, [DateTime]))
-    ,MONTH(DATEADD(mi, 5, [DateTime]))
-    ,DATEPART(dd, DATEADD(mi, 5, [DateTime]))
+	,DATEPART(QUARTER,DATEADD(M, 1, AnchorDate))
+    ,MONTH(DATEADD(M, 1, AnchorDate))
+    /* Not needed to this level of detail
+	,DATEPART(dd, DATEADD(mi, 5, [DateTime]))
     ,DATENAME(dw, DATEADD(mi, 5, [DateTime]))
     ,CASE
         WHEN DATENAME(dw, DATEADD(mi, 5, [DateTime])) LIKE 'S%'
@@ -164,15 +231,20 @@ SELECT -- Creating a new row for every 5 minute interval
      END as isweekday
 	 ,DATEPART(hh, DATEADD(mi, 5, [DateTime])) 
 	 ,DATEPART(mi, DATEADD(mi, 5, [DateTime])) 
+	 */
 FROM DatesUnion
-WHERE DATEADD(dd, 1, [DateTime]) < GETDATE()
+WHERE DATEADD(M, 1, AnchorDate) < GETDATE()
 )
-SELECT *
-INTO Dim.[DateTime]
+SELECT
+	[Year]
+	,[Quarter]
+	,Season
+	,[Month]
+INTO Dim.[Date]
 FROM DatesUnion
 OPTION (maxrecursion 0)
 -- Adding primary id key
-ALTER TABLE Dim.[DateTime]
+ALTER TABLE Dim.[Date]
 	Add id int identity primary key
 
 
@@ -182,19 +254,14 @@ ALTER TABLE Dim.[DateTime]
 ;with Years as (
 SELECT DISTINCT
 	[Year]
-FROM dim.[DateTime]
+FROM dim.[Date]
 WHERE [Year] > 2008
 )
 , ForceAreas as (
 SELECT DISTINCT
 	 [Force area]
 	,[Force code]
-FROM Dim.GeoEngland
-UNION
-SELECT DISTINCT
-	 [Force area]
-	,[Force code]
-FROM Dim.GeoWales
+FROM Dim.Geo
 )
 , DimStart as (
 SELECT
@@ -282,18 +349,13 @@ ALTER SCHEMA temp TRANSFER Drug.SurveyData
 ;with Years as (
 SELECT DISTINCT
 	[Year]
-FROM dim.[DateTime]
+FROM dim.[Date]
 )
 , Regions as (
 SELECT DISTINCT
 	 [Region name]
 	,[Region code]
-FROM Dim.GeoEngland
-UNION
-SELECT DISTINCT
-	 [Region name]
-	,[Region code]
-FROM Dim.GeoWales
+FROM Dim.Geo
 )
 , DimStart as (
 SELECT
@@ -339,7 +401,7 @@ ALTER TABLE Dim.YearlyRegion
 ;with Years as (
 SELECT DISTINCT
 	[Year]
-FROM dim.[DateTime]
+FROM dim.[Date]
 WHERE [Year] < 2018
 )
 ,KnifeAge as (
@@ -461,7 +523,225 @@ CROSS JOIN Temp.CrimeTypesOutcomes1
 ALTER TABLE Dim.CrimeTypesOutcomes
 	Add id int identity primary key
 
-/*-----------------------------
-    Creating Dim.
-------------------------------*/
 
+/*------------------------------------------------------
+    Prepping supplementary tables for YearlyForceArea
+-------------------------------------------------------*/
+-- Need to unpivot GunsAge and GunsGender, DrugSeizuresSnapshot should be easier
+CREATE SCHEMA Supp -- Supplementary
+
+-- Drug Seizures Snapshot
+SELECT
+	 ss.*
+	,yfa.id
+INTO Supp.DrugSeizuresSnapshot
+FROM drug.SeizuresSnapshot17 ss
+left join dim.YearlyForceArea yfa
+	ON Convert(int,Convert(varchar(100),ss.[Year])) = yfa.[Year] -- Matching on year
+	AND ss.[Area code] = yfa.[Force code] -- Also matching on force code
+
+-- Unpivoted Guns tables in Alteryx, now to join with YearlyForceArea and add foreign keys
+ALTER SCHEMA temp TRANSFER Supp.FirearmCertsByAge
+ALTER SCHEMA temp TRANSFER Supp.FirearmCertsByGender
+-- Age
+SELECT
+	 fca.*
+	,yfa.id
+INTO Supp.FirearmCertsByAge
+FROM Temp.FirearmCertsByAge fca
+left join dim.YearlyForceArea yfa
+	ON fca.[Year] = yfa.[Year] -- Matching on year
+	AND fca.[Force code] = yfa.[Force code] -- Also matching on force code
+-- Gender
+SELECT
+	 fcg.*
+	,yfa.id
+INTO Supp.FirearmCertsByGender
+FROM Temp.FirearmCertsByGender fcg
+left join dim.YearlyForceArea yfa
+	ON fcg.[Year] = yfa.[Year] -- Matching on year
+	AND fcg.[Force code] = yfa.[Force code] -- Also matching on force code
+
+-- Adding foreign keys to link to YearlyForceArea
+ALTER TABLE Supp.FirearmCertsByAge  
+ADD CONSTRAINT FK_FirearmCertsByAge_YearlyForceArea FOREIGN KEY (id)     
+    REFERENCES Dim.YearlyForceArea (id)
+ALTER TABLE Supp.FirearmCertsByGender 
+ADD CONSTRAINT FK_FirearmCertsByGender_YearlyForceArea FOREIGN KEY (id)     
+    REFERENCES Dim.YearlyForceArea (id)
+ALTER TABLE Supp.DrugSeizuresSnapshot
+ADD CONSTRAINT FK_DrugSeizuresSnapshot_YearlyForceArea FOREIGN KEY (id)     
+    REFERENCES Dim.YearlyForceArea (id)
+
+
+/*---------------------
+    FACT TABLE BABY!
+----------------------*/
+-- Rather than do a cte, Street table is much bigger so going to put into 'Temp'
+SELECT
+	 ms.*
+	,oo.[Outcome flag]
+	,cc.[Crime type] AS [New crime type]
+INTO temp.ReadyStreetData
+FROM police.MatchedStreet ms
+join (select distinct * from Temp.OutcomesToOutcomes) oo
+	on oo.[OG Outcome] =
+		CASE -- Fixing joins on NULLs
+		WHEN ms.[Last outcome category] IS NULL THEN 'NULL'
+		ELSE ms.[Last outcome category]
+	END
+join (select distinct * from Temp.CrimeToCrime) cc
+	on cc.[OG] =
+		CASE -- Fixing joins on NULLs
+		WHEN ms.[Crime type] IS NULL THEN 'NULL'
+		ELSE ms.[Crime type]
+	END
+
+-- Creating a cte to correctly place Outcomes and Crime Types in StopSearch
+;with crimetypeoutcome AS (
+SELECT
+	 ss.*
+	,oo.[Outcome flag]
+	,cc.[Crime type]
+FROM police.CrimeDataStopSearch ss
+join (select distinct * from Temp.OutcomesToOutcomes) oo
+	on oo.[OG Outcome] =
+		CASE -- Fixing joins on NULLs
+		WHEN ss.Outcome IS NULL THEN 'NULL'
+		ELSE ss.Outcome
+	END
+join (select distinct * from Temp.CrimeToCrime) cc
+	on cc.[OG] =
+		CASE -- Fixing joins on NULLs
+		WHEN ss.[Object of search] IS NULL THEN 'NULL'
+		ELSE ss.[Object of search]
+	END
+)
+-- Need to select both StopSearch and Street data in same format and union them
+, bigboyunion AS (
+-- StopSearch
+SELECT
+	 dd.id AS [Date id]
+	,g.id AS [Geo id]
+	,cto.id AS [Crime and outcome id]
+	,sse.id AS [Further details id]
+	,yfa.id AS [Yearly force area id]
+	,yr.id AS [Yearly region id]
+	,yn.id AS [Yearly nationwide id]
+	,p.id AS [Population id]
+FROM crimetypeoutcome ss
+left join dim.[DateTable] dd
+	on Year(ss.DateTimestamp) = dd.[Year]
+	AND Month(ss.DateTimestamp) = dd.[Month]
+left join dim.Geo g
+	on ss.[LSOA code] = g.[LSOA code]
+left join dim.CrimeTypesOutcomes cto
+	on CASE -- Fixing joins on NULLs
+		WHEN ss.[Crime type] = 'NULL' AND cto.[Crime type] IS NULL THEN 1
+		WHEN ss.[Crime type] = cto.[Crime type] THEN 1
+		ELSE 0
+	END = 1 
+	AND ss.[Outcome flag] = cto.[Outcome flag]
+left join dim.StopSearchExtras sse
+	on CASE -- Fixing joins on NULLs
+		WHEN ss.Gender IS NULL AND sse.Gender IS NULL THEN 1
+		WHEN ss.Gender = sse.Gender THEN 1
+		ELSE 0
+	END = 1
+	AND CASE -- Fixing joins on NULLs
+		WHEN ss.[Search type] IS NULL AND sse.[Search type] IS NULL THEN 1
+		WHEN ss.[Search type] = sse.[Search type] THEN 1
+		ELSE 0
+	END = 1
+	AND CASE -- Fixing joins on NULLs
+		WHEN ss.[Age range] IS NULL AND sse.[Age range] IS NULL THEN 1
+		WHEN ss.[Age range] = sse.[Age range] THEN 1
+		ELSE 0
+	END = 1
+	AND CASE -- Fixing joins on NULLs
+		WHEN ss.legislation IS NULL AND sse.legislation IS NULL THEN 1
+		WHEN ss.legislation = sse.legislation THEN 1
+		ELSE 0
+	END = 1
+left join dim.YearlyForceArea yfa
+	on Year(ss.DateTimestamp) = yfa.[Year]
+	AND g.[Force code] = yfa.[Force code]
+left join dim.YearlyRegion yr
+	on Year(ss.DateTimestamp) = yr.[Year]
+	AND g.[Region code] = yr.[Region code]
+left join dim.YearlyNationwide yn
+	on Year(ss.DateTimestamp) = yn.[Year]
+left join dim.pop p
+	on Year(ss.DateTimestamp) = Convert(int,Convert(varchar(100),p.[Year]))
+	AND ss.[LSOA code] = p.[LSOA code]
+
+UNION ALL
+
+-- Street
+SELECT
+	 dd.id AS [Date id]
+	,g.id AS [Geo id]
+	,cto.id AS [Crime and outcome id]
+	,295 AS [Further details id] -- no need to join here as Street doesn't have this data so just = NULL row
+	,yfa.id AS [Yearly force area id]
+	,yr.id AS [Yearly region id]
+	,yn.id AS [Yearly nationwide id]
+	,p.id AS [Population id]
+FROM temp.ReadyStreetData sd
+left join dim.[DateTable] dd
+	on sd.[Year] = dd.[Year]
+	AND sd.[Month] = dd.[Month]
+left join dim.Geo g
+	on sd.[LSOA code] = g.[LSOA code]
+left join dim.CrimeTypesOutcomes cto
+	on CASE -- Fixing joins on NULLs
+		WHEN sd.[New crime type] = 'NULL' AND cto.[Crime type] IS NULL THEN 1
+		WHEN sd.[New crime type] = cto.[Crime type] THEN 1
+		ELSE 0
+	END = 1 
+	AND sd.[Outcome flag] = cto.[Outcome flag]
+left join dim.YearlyForceArea yfa
+	on sd.[Year] = yfa.[Year]
+	AND g.[Force code] = yfa.[Force code]
+left join dim.YearlyRegion yr
+	on sd.[Year] = yr.[Year]
+	AND g.[Region code] = yr.[Region code]
+left join dim.YearlyNationwide yn
+	on sd.[Year] = yn.[Year]
+left join dim.pop p
+	on sd.[Year] = Convert(int,Convert(varchar(100),p.[Year]))
+	AND sd.[LSOA code] = p.[LSOA code]
+)
+SELECT
+	*
+INTO dim.FactTable
+FROM bigboyunion
+
+-- Adding foreign key constraints to link to Dim Tables
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_Date FOREIGN KEY ([Date id])     
+    REFERENCES Dim.[DateTable] (id)
+ALTER TABLE dim.FactTable 
+ADD CONSTRAINT FK_FactTable_Geo FOREIGN KEY ([Geo id])     
+    REFERENCES Dim.Geo (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_CrimeTypesOutcomes FOREIGN KEY ([Crime and outcome id])     
+    REFERENCES Dim.CrimeTypesOutcomes (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_FurtherDetails FOREIGN KEY ([Further details id])     
+    REFERENCES Dim.StopSearchExtras (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_YearlyForceArea FOREIGN KEY ([Yearly force area id])     
+    REFERENCES Dim.YearlyForceArea (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_YearlyRegion FOREIGN KEY ([Yearly region id])     
+    REFERENCES Dim.YearlyRegion (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_YearlyNationwide FOREIGN KEY ([Yearly nationwide id])     
+    REFERENCES Dim.YearlyNationwide (id)
+ALTER TABLE dim.FactTable
+ADD CONSTRAINT FK_FactTable_Population FOREIGN KEY ([Population id])     
+    REFERENCES Dim.Pop (id)
+-- Adding primary id key for fact table
+ALTER TABLE dim.FactTable
+	Add [Fact id] int identity primary key
